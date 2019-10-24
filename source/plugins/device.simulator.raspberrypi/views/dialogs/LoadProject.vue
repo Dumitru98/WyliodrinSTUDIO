982ed51a4f7df657db17d471e4b758955d582fb0
Alexandru-Călin DUMITRU (94702) <adumitru2712@stud.acs.upb.ro>
2019-10-24 17:04:08 +0300

    design needed to be done

--- source/plugins/device.simulator.raspberrypi/views/dialogs/LoadProject.vue
+++ source/plugins/device.simulator.raspberrypi/views/dialogs/LoadProject.vue
@@ -7,12 +7,14 @@
 		<v-progress-circular v-if="svgLoading" indeterminate color="red"></v-progress-circular>
 		<v-icon v-if="!svgNotLoaded && !svgLoading">mdi-checkbox-marked-circle</v-icon>
 		<v-btn v-if="!svgNotLoaded && !svgLoading" @click="removeSvg()"><v-icon>mdi-delete</v-icon></v-btn>
+		<v-text-field v-if="!svgNotLoaded && !svgLoading" :value="svgPath" label="SVG path" outlined readonly></v-text-field>
 
 		<v-btn color="secondary" dark @click="addXml()">Add Project XML</v-btn>
 		<v-icon v-if="xmlNotLoaded" @click="addXml()">mdi-file-plus</v-icon>
 		<v-progress-circular v-if="xmlLoading" indeterminate color="red"></v-progress-circular>
 		<v-icon v-if="!xmlNotLoaded && !xmlLoading">mdi-checkbox-marked-circle</v-icon>
 		<v-btn v-if="!xmlNotLoaded && !xmlLoading" @click="removeXml()"><v-icon>mdi-delete</v-icon></v-btn>
+		<v-text-field v-if="!xmlNotLoaded && !xmlLoading" :value="xmlPath" label="XML path" outlined readonly></v-text-field>
 
 		<v-alert v-if="showWarning" type="warning">{{ warning }}</v-alert>
 		<v-alert v-if="showError" type="error">{{ error }}</v-alert>
@@ -36,6 +38,9 @@ export default {
 			nameOwnProject: null,
 			svgOwnProjectString: null,
 			xmlOwnProjectString: null,
+
+			svgPath: null,
+			xmlPath: null,
 
 			svgDocument: null,
 			xmlParsed: null,
@@ -65,6 +70,8 @@ export default {
 			if (files.length) {
 				this.svgLoading = true;
 				this.svgNotLoaded = false;
+
+				this.svgPath = files[0].name;
 
 				let fileData = await this.studio.filesystem.readImportFile (files[0]);
 
@@ -101,6 +108,8 @@ export default {
 			if (files.length) {
 				this.xmlLoading = true;
 				this.xmlNotLoaded = false;
+
+				this.xmlPath = files[0].name;
 
 				let fileData = await this.studio.filesystem.readImportFile (files[0]);
 
